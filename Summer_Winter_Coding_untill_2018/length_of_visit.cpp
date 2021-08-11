fb02310d2eda9bf1da8e0d1576d94828157df567
highstar97 <pgt0185@gmail.com>
2021-08-11 19:00:35 +0900

    방문 길이 해결

--- Summer_Winter_Coding_untill_2018/length_of_visit.cpp
+++ Summer_Winter_Coding_untill_2018/length_of_visit.cpp
@@ -1,48 +1,85 @@
 #include <iostream>
 #include <string>
-#include <vector>
 #include <set>
 
-int solution(std::string dirs) {
-    int answer = 0;
-    std::vector<std::vector<std::set<std::string>>> matrix(11,std::vector<std::vector<std::string>>(11,std::set<std::string>()));
-    int x = 5, y = 5;
-    for(int i=0; i<dirs.length(); i++){
-        if( dirs[i] == 'U' ){
-            if( x-1 >= 0){
-                x--;
-                matrix[x][y].emplace("D");
-            }
-        }
-        else if( dirs[i] == 'D' ){
-            if( x+1 <= 11 ){
-                x++;
-                matrix[x][y].emplace("U");
-            }
-        }
-        else if( dirs[i] == 'L' ){
-            if( y-1 >= 0){
-                y--;
-                matrix[x][y].emplace("R");
-            }
-        }
-        else if( dirs[i] == 'R' ){
-            if( y+1 <= 11 ){
-                y++;
-                matrix[x][y].emplace("L");
-            }
-        }
-    }
+struct location{
+    int x,y;
+    bool operator<(const location& l) const;
+};
 
-    for(auto data : matrix){
-        for(auto data2 : data){
-            answer = data2.size();
+bool location::operator<(const location& l) const{
+    if(x != l.x)
+        return x < l.x;
+    return y < l.y;
+}
+
+int solution(std::string dirs) {
+    std::set<std::pair<location,location>> visited;
+
+    int answer = 0;
+    int nx = 0, ny = 0;
+    for(int i=0; i<dirs.length(); i++){
+        location l1, l2;
+        l1.x = nx, l1.y = ny;
+        switch(dirs[i]){
+            case 'U':{
+                l2.x = nx;
+                l2.y = ny+1;
+                if(l2.y <= 5){
+                    if(visited.find(std::make_pair(l1,l2)) == visited.end() && visited.find(std::make_pair(l2,l1)) == visited.end()){
+                        answer++;
+                        visited.emplace(std::make_pair(l1,l2));
+                        visited.emplace(std::make_pair(l2,l1));
+                    }
+                    ny++;
+                }
+                break;
+            }
+            case 'D':{
+                l2.x = nx;
+                l2.y = ny-1;
+                if(l2.y >= -5){
+                    if(visited.find(std::make_pair(l1,l2)) == visited.end() && visited.find(std::make_pair(l2,l1)) == visited.end()){
+                        answer++;
+                        visited.emplace(std::make_pair(l1,l2));
+                        visited.emplace(std::make_pair(l2,l1));
+                    }
+                    ny--;
+                }
+                break;
+            }
+            case 'L':{
+                l2.x = nx-1;
+                l2.y = ny;
+                if(l2.x >= -5){
+                    if(visited.find(std::make_pair(l1,l2)) == visited.end() && visited.find(std::make_pair(l2,l1)) == visited.end()){
+                        answer++;
+                        visited.emplace(std::make_pair(l1,l2));
+                        visited.emplace(std::make_pair(l2,l1));
+                    }
+                    nx--;
+                }
+                break;
+            }
+            case 'R':{
+                l2.x = nx+1;
+                l2.y = ny;
+                if(l2.x <= 5){
+                    if(visited.find(std::make_pair(l1,l2)) == visited.end() && visited.find(std::make_pair(l2,l1)) == visited.end()){
+                        answer++;
+                        visited.emplace(std::make_pair(l1,l2));
+                        visited.emplace(std::make_pair(l2,l1));
+                    }
+                    nx++;
+                }
+                break;
+            }
         }
     }
     return answer;
 }
 
 int main(){
-    std::string dirs = "ULURRDLLU";
+    std::string dirs = "LULLLLLLU";
     std::cout << solution(dirs);
 }
