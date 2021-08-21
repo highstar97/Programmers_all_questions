1d1ad4cc023e4a3d60c9a84cca0031680b177b44
highstar97 <pgt0185@gmail.com>
2021-08-22 00:18:44 +0900

    fail 카드 짝 맞추기

---
+++ 2021_KAKAO_BLIND_RECRUITMENT/Card_Matching_fail.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+int solution(std::vector<std::vector<int>> board, int r, int c) {
+    int answer = 0;
+    return answer;
+}
+
+int main(){
+    std::vector<std::vector<int>> board = {{1,0,0,3},{2,0,0,0},{0,0,0,2},{3,0,1,0}};
+    int r = 1, c = 0;
+    std::cout << solution(board,r,c);
+}
